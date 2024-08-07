7eff24b4a0f014fd83f1d2a3f1b1a2b98a926213
Alamdave Gill <alamdave@gmail.com>
2024-08-07 14:43:45 +0100

    Delete scripts directory

--- scripts/speedtest.sh
+++
@@ -1,8 +0,0 @@
-speedtest() {
-  if ! command -v speedtest-cli &> /dev/null; then
-    echo "speedtest-cli is not installed. Installing..."
-    sudo apt-get install -y speedtest-cli
-  fi
-
-  speedtest-cli --simple
-}
